8b187a5b234fbce2a86552a463f7e9849cc0c19f
paylik <paylik@yandex.ru>
2017-03-31 16:06:24 +0300

    111

--- 11/11(setTimeout выполнить функцию через 1 сек).js
+++ 11/11(setTimeout выполнить функцию через 1 сек).js
@@ -2,13 +2,13 @@
 for (var i = 0; i < 10; i++) {
     setTimeout(function (i) {
         console.log(i)
-    }, 10, i);
+    }, 1000, i);
 }
 //2 вариант
 for (var i = 0; i < 10; i++) {
     (function (i) {
         setTimeout(function () {
             console.log(i)
-        }, 10);
+        }, 1000);
     })(i);
 }
